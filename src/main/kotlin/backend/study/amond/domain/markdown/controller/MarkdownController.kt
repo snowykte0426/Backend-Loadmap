59f97fa994d6853d46946faf825ea0ae969361ab
snowykte0426 <snowykte0426@naver.com>
2024-08-26 22:47:40 +0900

    ✨feat: MarkdownController에 마크다운 파일 렌더링 기능 추가 및 MarkService 연동
    기존 `MarkdownController`를 수정하여 마크다운 파일을 로컬에서 읽어와 HTML로 렌더링하는 기능을 추가했습니다. 이를 위해 `MarkService`를 사용하여 마크다운 파일을 처리하고, 렌더링 결과를 모델에 추가하는 로직을 구현했습니다.

--- src/main/kotlin/backend/study/amond/domain/markdown/controller/MarkdownController.kt
+++ src/main/kotlin/backend/study/amond/domain/markdown/controller/MarkdownController.kt
@@ -1,23 +1,28 @@
 package backend.study.amond.domain.markdown.controller
 
+import backend.study.amond.domain.markdown.service.MarkService
+import org.commonmark.parser.Parser
+import org.commonmark.renderer.html.HtmlRenderer
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import kotlin.reflect.jvm.internal.impl.descriptors.Visibilities.Private
 
 @Controller
-class MarkdownController {
+class MarkdownController(private val markService: MarkService) {
 
     @GetMapping
     fun normal(): String {
-        return "redirect:"
+        return "redirect:/view/main"
     }
 
     @GetMapping("/view/{page}")
     fun markdownView(@PathVariable("page") page: String, model: Model): String {
-        val markdownValueFormLocal: String = getMarkdownValueFormLocal
-        // 추가적인 처리 로직 필요
-        return "markdownView"
+        val markdownValueFormLocal: String = markService.getMarkdownValueFormLocal(page)
+        val parser: Parser = Parser.builder().build()
+        val document = parser.parse(markdownValueFormLocal)
+        val renderer = HtmlRenderer.builder().build()
+        model.addAttribute("contents", renderer.render(document))
+        return "view"
     }
 }
