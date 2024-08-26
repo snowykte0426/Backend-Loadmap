97f60928fae75119ba35f1d56f314666dc1857a2
snowykte0426 <snowykte0426@naver.com>
2024-08-26 20:50:55 +0900

    🎉init: 프로젝트 초기 설정 및 기본 구조 구성
    새로운 프로젝트를 시작하면서 초기 설정을 완료하고, 기본적인 디렉토리 구조와 주요 파일들을 추가했습니다.

---
+++ src/test/kotlin/backend/study/amond/AmondApplicationTests.kt
@@ -0,0 +1,13 @@
+package backend.study.amond
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AmondApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
